f9e8be7354fa1529812875f903d5a5fb31d647e0
Will <willalexander2013@gmail.com>
2019-05-07 12:42:47 -0700

    Added constructor and set up priority queue with a comparator

--- com/willalexander/todolist/TodoList.java
+++ com/willalexander/todolist/TodoList.java
@@ -1,4 +1,7 @@
 package com.willalexander.todolist;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
 /**
  * 
  * @author William Alexander
@@ -6,4 +9,26 @@ package com.willalexander.todolist;
  */
 public class TodoList {
 
+	private PriorityQueue<Todo> queue;
+	private String name;
+	
+	TodoList(String name){
+		this.name = name;
+		this.queue = new PriorityQueue<>(50, new Comparator<Todo>() {
+
+			@Override
+			public int compare(Todo o1, Todo o2) {
+				if(o1.getPriority() < o2.getPriority()) {
+					return -1;
+				}else if(o1.getPriority() > o2.getPriority()) {
+					return 1;
+				}else {
+					return 0;
+				}
+			}
+			
+			
+		});
+	}
+	
 }
