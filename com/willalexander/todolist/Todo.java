209ebf790dfe5fca56a3263a88d15791b774561c
Will <willalexander2013@gmail.com>
2019-05-07 12:16:57 -0700

    Added instance variables; made constructor

--- com/willalexander/todolist/Todo.java
+++ com/willalexander/todolist/Todo.java
@@ -7,4 +7,23 @@ package com.willalexander.todolist;
  */
 public class Todo {
 
+	private String description;
+	private int date;
+	private int priority;
+	private TodoStatus status;
+	
+	/**
+	 * 
+	 * @param description String represents a description of the task
+	 * @param date Int represents the unix due date of a task
+	 * @param priority Int represents the importance compared to other tasks
+	 * Constructor default status is set to NOTSTARTED
+	 */
+	Todo(String description, int date, int priority){
+		this.description = description;
+		this.date = date;
+		this.priority = priority;
+		this.status = TodoStatus.NOTSTARTED;
+	}
+
 }
