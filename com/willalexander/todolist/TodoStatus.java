6bf956ede0c2bbdfdc9b1b31c0a28c298834a9b5
Will <willalexander2013@gmail.com>
2019-05-07 12:21:04 -0700

    Added copyright message to TodoStatus

--- com/willalexander/todolist/TodoStatus.java
+++ com/willalexander/todolist/TodoStatus.java
@@ -1,5 +1,9 @@
 package com.willalexander.todolist;
-
+/**
+ * 
+ * @author William Alexander
+ * Copyright 2019 All rights reserved
+ */
 public enum TodoStatus {
 	NOTSTARTED, INPROGRESS, COMPLETE;
 }
